91a903ee856eb8a50bd70597d137a3ecf45a4440
Tayyab <F2017065009@umt.edu.pk>
2022-04-14 20:55:22 +0500

    admin email field

--- account/admin.py
+++ account/admin.py
@@ -3,8 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from account.models import Account
 
 
-
 class AccountAdmin(UserAdmin):
+
+	add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
 	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff','is_dentist','is_reception','is_nurse')
 	search_fields = ('email','username',)
 	readonly_fields=('id', 'date_joined', 'last_login')
